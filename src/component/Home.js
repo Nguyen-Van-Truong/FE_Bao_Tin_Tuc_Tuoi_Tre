876b1d30df359ff9dc2751817eee61cc81895ca9
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2023-06-07 16:19:09 +0700

    merge code

--- src/component/Home.js
+++ src/component/Home.js
@@ -68,76 +68,70 @@ function Home() {
     return (
         <div>
             <Header/>
+            <MainNews items={items}/>
+            <CategoryRow items={worldItems} title={categories.length > 0 ? categories[1].title : 'Đang tải...'}/>
 
-            <MainNews items={items} />
-            <CategoryRow items={worldItems} title={categories.length > 0 ? categories[1].title : 'Đang tải...'} />
-
-            {/*Tìm kiếm tin tức*/}
-            <div className="search-container">
-                <input
-                    type="text"
-                    placeholder="Tìm kiếm tin tức..."
-                    value={searchTerm}
-                    onChange={e => setSearchTerm(e.target.value)}
-                />
-                <button onClick={handleSearch}>
-                    Tìm kiếm
-                </button>
-            </div>
-
-            {/*xử lý tìm kiếm tin tức*/}
-            {searchResults.length > 0 && (
-                <Section title="Kết quả tìm kiếm">
-                    {searchResults.map((item, index) => (
-                        <div className="row mb-3 bb-1 pt-0" key={index}>
-                            <div className="col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                                <img className="thumb" src={item.img} alt="Ảnh minh họa bài viết" />
-                            </div>
-                            <div className="col-md-8 col-lg-8 col-sm-12 col-xs-12">
-                                <h5>
-                                    <a href={`detail?url=${encodeURIComponent(item.link)}`}>{item.title}</a>
-                                </h5>
-                                <small>{item.pubDate}</small>
-                                <p className="summary pt-3">{item.description}</p>
-                            </div>
+            <div className="container section">
+                <div className="row">
+                    {/* "Cập nhật mới nhất" section */}
+                    <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                        {/* Tìm kiếm tin tức */}
+                        <div className="search-container">
+                            <input
+                                type="text"
+                                placeholder="Tìm kiếm tin tức..."
+                                value={searchTerm}
+                                onChange={e => setSearchTerm(e.target.value)}
+                            />
+                            <button onClick={handleSearch}>Tìm kiếm</button>
                         </div>
-                    ))}
-                </Section>
-            )}
 
-            {/*Cập nhật mới nhất*/}
-            <Section title="Cập nhật mới nhất">
-                {searchResults.length > 0 && (
-                    <Section title="Kết quả tìm kiếm">
-                        {searchResults.map((item, index) => (
-                            <div className="row mb-3 bb-1 pt-0" key={index}>
-                                {/* Phần hiển thị nội dung tin tức tương tự như trong Section hiện tại */}
-                            </div>
-                        ))}
-                    </Section>
-                )}
-                {items.map((item, index) => (
-                    <div className="row mb-3 bb-1 pt-0" key={index}>
-                        <div className="col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                            <img className="thumb" src={item.img} alt="Article Thumbnail"/>
-                        </div>
-                        <div className="col-md-8 col-lg-8 col-sm-12 col-xs-12">
-                            <h5>
-                                <a href={`detail?url=${encodeURIComponent(item.link)}`}>{item.title}</a>
-                            </h5>
-                            <small>{item.pubDate}</small>
-                            <p className="summary pt-3">{item.description}</p>
-                        </div>
+                        {/* Kết quả tìm kiếm */}
+                        {searchResults.length > 0 && (
+                            <Section title="Kết quả tìm kiếm">
+                                {searchResults.map((item, index) => (
+                                    <div className="row mb-3 bb-1 pt-0" key={index}>
+                                        <div className="col-md-4 col-lg-4 col-sm-12 col-xs-12">
+                                            <img className="thumb" src={item.img} alt="Ảnh minh họa bài viết" />
+                                        </div>
+                                        <div className="col-md-8 col-lg-8 col-sm-12 col-xs-12">
+                                            <h5>
+                                                <a href={`detail?url=${encodeURIComponent(item.link)}`}>{item.title}</a>
+                                            </h5>
+                                            <small>{item.pubDate}</small>
+                                            <p className="summary pt-3">{item.description}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </Section>
+                        )}
+
+                        <Section title="Cập nhật mới nhất">
+                            {items.map((item, index) => (
+                                <div className="row mb-3 bb-1 pt-0" key={index}>
+                                    <div className="col-md-4 col-lg-4 col-sm-12 col-xs-12">
+                                        <img className="thumb" src={item.img} alt="Article Thumbnail"/>
+                                    </div>
+                                    <div className="col-md-8 col-lg-8 col-sm-12 col-xs-12">
+                                        <h5>
+                                            <a href={`detail?url=${encodeURIComponent(item.link)}`}>{item.title}</a>
+                                        </h5>
+                                        <small>{item.pubDate}</small>
+                                        <p className="summary pt-3">{item.description}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </Section>
                     </div>
-                ))}
-            </Section>
 
-            <Section title="Technology">
-                <Trending items={technologyItems.slice(0, 4)}/>
-            </Section>
+                    {/* "Trending" section */}
+                    <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                        <Trending items={technologyItems.slice(0, 4)}/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
-
 }
 
 export default Home;
