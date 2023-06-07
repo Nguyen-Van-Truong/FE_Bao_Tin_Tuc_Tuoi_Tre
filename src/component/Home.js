f0ec0c0003788700a01c69e998c5c81733e9074d
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2023-06-07 14:39:44 +0700

    Sửa lại phần giao diện xu hướng

--- src/component/Home.js
+++ src/component/Home.js
@@ -36,31 +36,40 @@ function Home() {
     return (
         <div>
             <Header/>
-            <MainNews items={items} />
-            <CategoryRow items={worldItems} title={categories.length > 0 ? categories[1].title : 'Đang tải...'} />
+            <MainNews items={items}/>
+            <CategoryRow items={worldItems} title={categories.length > 0 ? categories[1].title : 'Đang tải...'}/>
 
-            <Section title="Cập nhật mới nhất">
-                {items.map((item, index) => (
-                    <div className="row mb-3 bb-1 pt-0" key={index}>
-                        <div className="col-md-4 col-lg-4 col-sm-12 col-xs-12">
-                            <img className="thumb" src={item.img} alt="Article Thumbnail"/>
-                        </div>
-                        <div className="col-md-8 col-lg-8 col-sm-12 col-xs-12">
-                            <h5>
-                                <a href={`detail?url=${encodeURIComponent(item.link)}`}>{item.title}</a>
-                            </h5>
-                            <small>{item.pubDate}</small>
-                            <p className="summary pt-3">{item.description}</p>
-                        </div>
+            <div className="container section">
+                <div className="row">
+                    {/* "Cập nhật mới nhất" section */}
+                    <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                        <Section title="Cập nhật mới nhất">
+                            {items.map((item, index) => (
+                                <div className="row mb-3 bb-1 pt-0" key={index}>
+                                    <div className="col-md-4 col-lg-4 col-sm-12 col-xs-12">
+                                        <img className="thumb" src={item.img} alt="Article Thumbnail"/>
+                                    </div>
+                                    <div className="col-md-8 col-lg-8 col-sm-12 col-xs-12">
+                                        <h5>
+                                            <a href={`detail?url=${encodeURIComponent(item.link)}`}>{item.title}</a>
+                                        </h5>
+                                        <small>{item.pubDate}</small>
+                                        <p className="summary pt-3">{item.description}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </Section>
                     </div>
-                ))}
-            </Section>
 
-            <Section title="Technology">
-                <Trending items={technologyItems.slice(0, 4)}/>
-            </Section>
+                    {/* "Trending" section */}
+                    <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                        <Trending items={technologyItems.slice(0, 4)}/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
+
 }
 
 export default Home;
